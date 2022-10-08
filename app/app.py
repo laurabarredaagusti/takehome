2f88a47c8a7f8d833efc1203f82889ea65ce622c
lauragreemko <laura.barreda@greemko.com>
2022-10-08 22:48:13 +0200

    created image

---
+++ app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import pandas as pd
+from os import environ
+from functions import *
+
+app = app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def hello():
+    return render_template('index.html')
+
+# 1. Devolver la predicción de los nuevos datos enviados mediante argumentos en la llamada
+@app.route('/predict', methods=['GET'])
+def predict():
+    model = load_models('sentiment_model')
+    text = get_arguments('text')
+    df = pd.DataFrame()
+    df['text'] = [text]
+    prediction = model.predict(df['text'])
+    prediction = prediction[0]
+    return render_template('predict.html', predict=prediction)
+
+if __name__ == '__main__':
+  app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
---
+++ app/functions.py
@@ -0,0 +1,9 @@
+import pickle
+from flask import request
+
+def load_models(model_name):
+    path = 'static/models/' + model_name
+    return pickle.load(open(path,'rb'))
+
+def get_arguments(arg):
+    return request.args.get(arg, None)
