58a0b6539cec0d03d5dece42d81c7697378c0b28
lauragreemko <laura.barreda@greemko.com>
2022-10-08 22:54:00 +0200

    changed run

--- app/app.py
+++ app/app.py
@@ -20,5 +20,7 @@ def predict():
     prediction = prediction[0]
     return render_template('predict.html', predict=prediction)
 
-if __name__ == '__main__':
-  app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
+# if __name__ == '__main__':
+#   app.run(debug = True, host = '0.0.0.0', port=environ.get("PORT", 5000))
+
+app.run()
